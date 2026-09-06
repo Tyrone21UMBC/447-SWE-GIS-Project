5c4303fc361d8b533af82d30923852af62db5a6a
Tyrone21UMBC <romaind1@umbc.edu>
2026-09-06 01:01:31 -0400

    created the login screen routing page function in main.py

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+# home page
 @app.get("/")
 async def read_root():
     return {"message": "Hello!"}
+
+# this will be a sub page for the login screen
+@app.get("/login/")
+def login_screen():
+    return {"message": "Login screen"}
+
 
